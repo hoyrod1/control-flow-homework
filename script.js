dd1616498736294f0568c6dd9f3f521d93292336
Rodney St Cloud <hoyrod1@aol.com>
2024-04-12 17:37:20 -0400

    Pruned the plant from exceeding 80% of capacity

--- script.js
+++ script.js
@@ -64,20 +64,63 @@ console.log(
 const plantGrowthDoublePerWk = 2 * currentAmountOfPlants;
 console.log(`The plant size = ${plantGrowthDoublePerWk} every week`);
 /**********************************************************************/
+/**********************************************************************/
 console.log(
-  "===================== Plant Growth after 4 weeks ======================="
+  "=================== Plant Growth for 4 weeks ====================="
 );
+
+// The predicted Plants Growth for 4 weeks is 80
 const weeks = 4;
 let count = 1;
 while (count <= weeks) {
+  let newAmountOfPlants = currentAmountOfPlants * count;
+  const currentArea4TwentyPlants = newAmountOfPlants * area4OnePlant;
   console.log(
     `The predicted plant growth after week ${count} is ${Math.floor(
-      currentAmountOfPlants * count
-    )}`
+      newAmountOfPlants
+    )} and the area that ${Math.floor(
+      newAmountOfPlants
+    )} plants requires is ${Math.floor(
+      currentArea4TwentyPlants
+    )} square meters.`
   );
   count++;
 }
 
 console.log(
-  "========================================================================"
+  "==================================================================="
 );
+/**********************************************************************/
+console.log(
+  "=================== Plant Pruned For 4 weeks ====================="
+);
+let currentAreaPercentage4Plants = area * 0.8;
+const weeksb = 4;
+let countb = 1;
+
+console.log(
+  `80% of ${Math.floor(area)} square meters is ${Math.floor(
+    currentAreaPercentage4Plants
+  )} quare meters.`
+);
+
+while (countb <= weeks) {
+  let newAmountOfPlants = currentAmountOfPlants * countb;
+  const currentArea4TwentyPlants = newAmountOfPlants * area4OnePlant;
+  if (currentArea4TwentyPlants > currentAreaPercentage4Plants) {
+    console.log(
+      `After week ${countb} the amount of ${Math.floor(
+        newAmountOfPlants
+      )} plants covers the area of ${Math.floor(
+        currentArea4TwentyPlants
+      )} square meters which is greater than 80% of capacity`
+    );
+    break;
+  }
+  countb++;
+}
+
+console.log(
+  "==================================================================="
+);
+/**********************************************************************/
