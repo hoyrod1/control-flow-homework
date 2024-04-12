cb5a5603174ae9fa8534e3a8b8ff96835c2a2f73
Rodney St Cloud <hoyrod1@aol.com>
2024-04-12 13:26:25 -0400

    Last message had wrong message. The correct message is the minimum area a single plant required and for this commit I have calculated the area that 20 plants require

--- script.js
+++ script.js
@@ -2,12 +2,41 @@ console.log("Hello World");
 console.log(
   "-----------------------------------------------------------------"
 );
+// The constance PI
 const PI = 3.1415;
 
+/**********************************************************************/
+// The radius cotaining the plants is 5(5) meters
 let radius = 5;
-
+// The calculation of the (area) containing the plants is 79
 const area = PI * radius * radius;
-
 console.log(`The area which contains the plants is ${Math.ceil(area)}`);
+/**********************************************************************/
+
+/**********************************************************************/
+// Currently the plants requires a minimum space of 0.8 square meters
+const squareMeters = 0.8;
+// The calculation of the minimum (area) that each plant require is 3
+const area4OnePlant = PI * squareMeters;
+console.log(
+  `The size of the area that each plant requires is ${Math.ceil(area4OnePlant)}`
+);
+/**********************************************************************/
+
+/**********************************************************************/
+// The area is starting with 20 plants.
+let currentAmountOfPlants = 20;
+// The calculation of the (area) that twenty plant require is 51
+const currentArea4TwentyPlants = currentAmountOfPlants * area4OnePlant;
+console.log(
+  `The size of the area that twenty plants require is ${Math.ceil(
+    currentArea4TwentyPlants
+  )}`
+);
+/**********************************************************************/
+
+// The plants double in number every week
+const plantSizeGrowth = 2;
+
 // console.log(`The constant, pi = ${PI}`);
 // console.log(`The radius, squared = ${radius}`);
