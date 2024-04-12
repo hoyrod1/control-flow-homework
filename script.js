407d5c7daf10ce4691d1c2fd9e8b16d76f813f8a
Rodney St Cloud <hoyrod1@aol.com>
2024-04-12 11:52:22 -0400

    Calculated the area that contains the plants

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+console.log(
+  "-----------------------------------------------------------------"
+);
+const PI = 3.1415;
+
+let radius = 5;
+
+const area = PI * radius * radius;
+
+console.log(`The area which contains the plants is ${Math.ceil(area)}`);
+// console.log(`The constant, pi = ${PI}`);
+// console.log(`The radius, squared = ${radius}`);
