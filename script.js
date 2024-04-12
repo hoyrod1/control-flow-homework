a4348cf4ba77c8f01220315fbdc979be38cdfa9b
Rodney St Cloud <hoyrod1@aol.com>
2024-04-12 16:25:07 -0400

    Predicted the plant growth for 4 weeks

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 console.log("Hello World");
+/**********************************************************************/
+// The constance PI
+const PI = 3.1415;
+
+// Months Per Year
+const monthPerYear = 12;
+
+// Weeks Per Year
+const weeksPerYear = 52;
+
+// Days Per Year
+const daysPerYear = 365;
+
+// Days Per Week
+const dayCount = daysPerYear / weeksPerYear;
+
+/**********************************************************************/
+
+/************************* DAYS IN A WEEKS ****************************/
 console.log(
   "-----------------------------------------------------------------"
 );
-// The constance PI
-const PI = 3.1415;
+
+console.log(`There are ${Math.floor(dayCount)} days in a week`);
+
+console.log(
+  "-----------------------------------------------------------------"
+);
+/**********************************************************************/
 
 /**********************************************************************/
 // The radius cotaining the plants is 5(5) meters
 let radius = 5;
 // The calculation of the (area) containing the plants is 79
 const area = PI * radius * radius;
-console.log(`The area which contains the plants is ${Math.ceil(area)}`);
+console.log(`The area which contains the plants is ${Math.floor(area)}`);
 /**********************************************************************/
 
 /**********************************************************************/
@@ -19,7 +43,9 @@ const squareMeters = 0.8;
 // The calculation of the minimum (area) that each plant require is 3
 const area4OnePlant = PI * squareMeters;
 console.log(
-  `The size of the area that each plant requires is ${Math.ceil(area4OnePlant)}`
+  `The size of the area that each plant requires is ${Math.floor(
+    area4OnePlant
+  )}`
 );
 /**********************************************************************/
 
@@ -29,14 +55,29 @@ let currentAmountOfPlants = 20;
 // The calculation of the (area) that twenty plant require is 51
 const currentArea4TwentyPlants = currentAmountOfPlants * area4OnePlant;
 console.log(
-  `The size of the area that twenty plants require is ${Math.ceil(
+  `The size of the area that twenty plants require is ${Math.floor(
     currentArea4TwentyPlants
   )}`
 );
 /**********************************************************************/
+// The plants size double in number every week
+const plantGrowthDoublePerWk = 2 * currentAmountOfPlants;
+console.log(`The plant size = ${plantGrowthDoublePerWk} every week`);
+/**********************************************************************/
+console.log(
+  "===================== Plant Growth after 4 weeks ======================="
+);
+const weeks = 4;
+let count = 1;
+while (count <= weeks) {
+  console.log(
+    `The predicted plant growth after week ${count} is ${Math.floor(
+      currentAmountOfPlants * count
+    )}`
+  );
+  count++;
+}
 
-// The plants double in number every week
-const plantSizeGrowth = 2;
-
-// console.log(`The constant, pi = ${PI}`);
-// console.log(`The radius, squared = ${radius}`);
+console.log(
+  "========================================================================"
+);
